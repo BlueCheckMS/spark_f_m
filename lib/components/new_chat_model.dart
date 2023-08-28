e232231f8207e0678708ec507f3edebf1c15534a
Antivery <antivery@gmail.com>
2023-08-28 14:13:00 -0400

    new

---
+++ lib/components/new_chat_model.dart
@@ -0,0 +1,38 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class NewChatModel extends FlutterFlowModel {
+  ///  Local state fields for this component.
+
+  String? chatName = '';
+
+  ///  State fields for stateful widgets in this component.
+
+  // Stores action output result for [Firestore Query - Query a collection] action in newChat widget.
+  List<UsersRecord>? users;
+  // State field(s) for TextField widget.
+  TextEditingController? textController;
+  String? Function(BuildContext, String?)? textControllerValidator;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {
+    textController?.dispose();
+  }
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
